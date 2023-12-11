f55848b066ebbe52f780c5a3f159839c7736ca16
Marcell Perger <marcell.perger.1@gmail.com>
2023-12-11 21:37:10 +0000

    Do day 10 part 1

--- problem_10/src/main.rs
+++ problem_10/src/main.rs
@@ -1,7 +1,9 @@
+use itertools::FoldWhile;
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs;
 use std::iter;
+use std::mem;
 
 fn main() {
     part1();
@@ -72,6 +74,7 @@ impl TileType {
     }
 }
 
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Pos {
     lni: usize,
@@ -86,37 +89,40 @@ impl Pos {
         grid[self.lni][self.xi]
     }
 
-    fn get_adj(self, last_pos_excl: Pos) -> Vec<Pos> {
-        let last_pos_incl = Pos::from_ln_x(last_pos_excl.lni - 1, last_pos_excl.xi - 1);
-        let mut out: Vec<Pos> = Vec::with_capacity(4);
-        if self.lni > 0 {
-            out.push(Pos::from_ln_x(self.lni - 1, self.xi));
+    fn add_dirn_unsafe(self, dirn: Dirn) -> Self {
+        use Dirn::*;
+        match dirn {
+            N => Self { lni: self.lni - 1, xi: self.xi },
+            S => Self { lni: self.lni + 1, xi: self.xi },
+            E => Self { lni: self.lni, xi: self.xi + 1 },
+            W => Self { lni: self.lni, xi: self.xi - 1 }
         }
-        if self.lni < last_pos_incl.lni {  // if this isn't last
-            out.push(Pos::from_ln_x(self.lni + 1, self.xi))
-        }
-        if self.xi > 0 {
-            out.push(Pos::from_ln_x(self.lni, self.xi -1 ));
-        }
-        if self.xi < last_pos_incl.xi {  // if this isn't last
-            out.push(Pos::from_ln_x(self.lni, self.xi +1 ))
-        }
-        out
     }
+    fn add_dirn(self, dirn: Dirn, gsize: Pos) -> Result<Self, &'static str> {
+        use Dirn::*;
+        let Self {lni, xi} = self;
+        match dirn {
+            N => self.lni.checked_sub(1).and_then(|lni| Some(Self {lni, xi})).ok_or("Cannot access N direction (outside of the map)"),
+            S => (lni + 1 < gsize.lni).then_some(Self { lni: lni + 1, xi }).ok_or("Cannot access S direction (outside of the map)"),
+            E => (xi + 1 < gsize.xi).then_some(Self { lni, xi: xi + 1 }).ok_or("Cannot access E direction (outside of the map)"),
+            W => self.xi.checked_sub(1).and_then(|xi| Some(Self {lni, xi})).ok_or("Cannot access W direction (outside of the map)")
+        }
+    }
+
     fn get_adj_and_dirn(self, last_pos_excl: Pos) -> Vec<(Dirn, Pos)> {
         let last_pos_incl = Pos::from_ln_x(last_pos_excl.lni - 1, last_pos_excl.xi - 1);
         let mut out: Vec<(Dirn, Pos)> = Vec::with_capacity(4);
         if self.lni > 0 {
-            out.push((Dirn::E, Pos::from_ln_x(self.lni - 1, self.xi)));
+            out.push((Dirn::N, Pos::from_ln_x(self.lni - 1, self.xi)));
         }
         if self.lni < last_pos_incl.lni {  // if this isn't last
-            out.push((Dirn::W, Pos::from_ln_x(self.lni + 1, self.xi)));
+            out.push((Dirn::S, Pos::from_ln_x(self.lni + 1, self.xi)));
         }
         if self.xi > 0 {
-            out.push((Dirn::N, Pos::from_ln_x(self.lni, self.xi -1)));
+            out.push((Dirn::W, Pos::from_ln_x(self.lni, self.xi -1)));
         }
         if self.xi < last_pos_incl.xi {  // if this isn't last
-            out.push((Dirn::S, Pos::from_ln_x(self.lni, self.xi +1)));
+            out.push((Dirn::E, Pos::from_ln_x(self.lni, self.xi +1)));
         }
         out
     }
@@ -133,12 +139,43 @@ fn find_start(grid: &Vec<Vec<TileType>>) -> Pos {
 }
 
 // "always two [connecting tiles] there are, no more, no less"
-fn find_connecting_to_start(grid: &Vec<Vec<TileType>>, start_pos: Pos, gsize: Pos) -> (Pos, Pos) {
+fn find_connecting_to_start(grid: &Vec<Vec<TileType>>, start_pos: Pos, gsize: Pos) -> [(Dirn, Pos); 2] {
     start_pos.get_adj_and_dirn(gsize)
         .into_iter()
-        .filter_map(|(dirn_from_start, pos)| pos.index_in(grid).has_connector(dirn_from_start.opp()).then_some(pos))
-        .collect_tuple()
-        .expect("always two [connection to the start] there should be, no more, no less")
+        .filter_map(|(dirn_from_start, pos)| pos.index_in(grid).has_connector(dirn_from_start.opp()).then_some((dirn_from_start, pos)))
+        .collect_vec()
+        .try_into()
+        .expect("always two [connection to the start] there are, no more, no less")
+}
+
+fn find_next(grid: &Vec<Vec<TileType>>, (dirn_from_prev, pos): (Dirn, Pos), gsize: Pos) -> (Dirn, Pos) {
+    let dirn_to_prev = dirn_from_prev.opp();
+    let curr_tile = pos.index_in(grid);
+    let curr_connectors = curr_tile.get_connector_pair().expect("find_next should only be called on a normal tile");
+    let dirn_to_next = curr_connectors.iter().copied().filter(|c| *c != dirn_to_prev).exactly_one()
+        .expect("Each connector should have 2 connections: 1 to the prev, 1 to next");
+    // println!("find_next: {:?};   {:?}", dirn_to_next, pos); 
+    let next_pos = pos.add_dirn(dirn_to_next, gsize).expect("Error: pipe is pointing out of map");
+    (dirn_to_next, next_pos)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum LoopControl {
+    Continue, Break
+}
+
+#[must_use="Should check to end loop or not"]
+fn handle_next_node(grid: &Vec<Vec<TileType>>, curr_info: &mut ((Dirn, Pos), usize), dist_map: &mut HashMap<Pos, usize>, gsize: Pos) -> LoopControl {
+    // println!("{:?};   {:?};   {:?}", curr_info, dist_map, gsize);
+    // this mem::replace is sorta hacky but whatev
+    let _ = mem::replace(curr_info, (find_next(&grid, curr_info.0, gsize), curr_info.1 + 1));
+    // if it is in, already found by traversing other so finished, ...
+    if dist_map.contains_key(&curr_info.0.1) {
+        return LoopControl::Break;
+    }
+    // ... else, add it and continue
+    dist_map.insert(curr_info.0.1, curr_info.1);
+    LoopControl::Continue
 }
 
 fn part1() {
@@ -149,10 +186,19 @@ fn part1() {
         .map(|x| x.trim())
         .filter(|x| x.len() > 0);
     let grid = lines.map(parse_line).collect_vec();
+    // println!("{}", grid.iter().map(|ln| format!("{:?}", ln)).join("\n"));
     let gsize = Pos::from_ln_x(grid.len(), grid[0].len());
     let start_pos = find_start(&grid);
     let start_adj = find_connecting_to_start(&grid, start_pos, gsize);
-    let dist_map: HashMap<Pos, usize> = HashMap::from([(start_pos, 0), (start_adj.0, 1), (start_adj.1, 1)]);
-    let curr = start_adj;
-    // TODO: while ! next_pos in dist_map: expand by 1 from curr - find thing in dirn of conn
+    // rather unidiomatic rust but whatever... (should've / could've used fold_while)
+    let mut dist_map: HashMap<Pos, usize> = HashMap::from([(start_pos, 0), (start_adj[0].1, 1), (start_adj[1].1, 1)]);
+    let mut curr = start_adj.map(|v| (v, 1));
+    loop {
+        if handle_next_node(&grid, &mut curr[0], &mut dist_map, gsize) == LoopControl::Break { break; }
+        if handle_next_node(&grid, &mut curr[1], &mut dist_map, gsize) == LoopControl::Break { break; }
+    }
+    // println!("{:?}", dist_map);
+    let m = dist_map.into_iter().max_by_key(|(_pos, dist)| *dist).unwrap();
+    println!("Part 1: {} at {:?}", m.1, m.0);
+    // TODO
 }
