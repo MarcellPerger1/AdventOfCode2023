e9e0397504416fd17700d49907476587df583071
Marcell Perger <marcell.perger.1@gmail.com>
2023-12-16 21:42:18 +0000

    Remove the deubgging

--- problem_12/src/main.rs
+++ problem_12/src/main.rs
@@ -43,9 +43,7 @@ fn parse_line(line: &str) -> Line {
     Line { states: parse_states(state_s), nums: parse_num_list(nums_s).collect_vec() }
 }
 
-fn matches_states(expected_v: &[State], actual_v: &[State], dp: usize) -> bool {
-    let indent = " ".repeat(dp);
-    // println!("{indent}  expect: {:?}\n{indent}  actual: {:?}", expected_v, actual_v);
+fn matches_states(expected_v: &[State], actual_v: &[State]) -> bool {
     expected_v.iter().zip_eq(actual_v).all(|(expect, actual)| *expect == State::Unknown || *actual == State::Unknown || *expect == *actual)
 }
 
@@ -55,15 +53,12 @@ fn get_combs_nolengths(states: &[State]) -> usize {
     if states.iter().all(|s| *s != State::Broken) { 1 } else { 0 }
 }
 
-fn get_combs(states: &[State], lengths: &[usize], dp: usize) -> usize {
-    let indent = " ".repeat(dp);
-    // println!("{indent}states: {:?};   lengths: {:?}", states, lengths);
+fn get_combs(states: &[State], lengths: &[usize]) -> usize {
     let (&len_curr, lengths_rest) = match lengths.split_first() {
         None => return get_combs_nolengths(states),
         Some(v) => v
     };
     if len_curr > states.len() {  // impossible to fulfill so return 0
-        // println!("{indent}Ret: 0 [LenTooBig]");
         return 0;
     }
     if len_curr == states.len() {
@@ -72,11 +67,9 @@ fn get_combs(states: &[State], lengths: &[usize], dp: usize) -> usize {
         if lengths_rest.len() > 0 {
             // has more other than this whcih won't have any states left to satisfy them
             // (this one uses up all the states until the end)
-            // println!("{indent}Ret: 0 [SameLen,HasMore]");
             return 0;
         }
         let r = if states.iter().all(|s| *s != State::Normal) { 1 } else { 0 };
-        // println!("{indent}Ret: {} [SameLen]", r);
         return r;
     }
     let actual_states_sl = [State::Broken].repeat(len_curr).into_iter().chain(iter::once(State::Normal)).collect_vec();
@@ -89,9 +82,7 @@ fn get_combs(states: &[State], lengths: &[usize], dp: usize) -> usize {
         .and_then(|(i, _)| Some(i))
         .unwrap_or(usize::MAX);  // won't be selected by min
     let starts =  0..=(states.len() - len_curr).min(first_broken);
-    // println!("{indent}{:?}", starts);
     let possiblities_it = starts.filter_map(|start| {
-        // println!("{indent}start: {}", start);
         let end_excl_no_trailing_normal = start + len_curr;
         if end_excl_no_trailing_normal == states.len() {
             // this is the case where the pattern itself fits, 
@@ -99,41 +90,32 @@ fn get_combs(states: &[State], lengths: &[usize], dp: usize) -> usize {
             // (there can't be extra Broken's in OOB)
             let end_excl = end_excl_no_trailing_normal;
             let expected_sl = &states[start..end_excl];
-            if !matches_states(expected_sl, &[State::Broken].repeat(len_curr), dp) {
-                // println!("{indent}  Doesn't match, discard [AtEnd]");
+            if !matches_states(expected_sl, &[State::Broken].repeat(len_curr)) {
                 // Can't place it here as it would break the states_list
                 return None;
             }
             // states_rest = []
             // so only return 1 if no more lengths to match (if there are, we have no states to satisfy them so return None)
             if lengths_rest.len() == 0 {
-                // println!("{indent}  Matches [AtEnd,NoMore] - return 1 -");
-                return Some(1); 
+                return Some(1);
             } else {
-                // println!("{indent}  Matches but has more lengths without more states [AtEnd,NeedsMore]");
                 return None;
             }
         }
         let end_excl = start + len_curr + 1;  // + 1 to include extra normal at end of actual states
         let expected_sl = &states[start..end_excl];
-        if !matches_states(expected_sl, &actual_states_sl, dp) {
-            // println!("{indent}  Doesn't match, discard");
+        if !matches_states(expected_sl, &actual_states_sl) {
             // Can't place it here as it would break the states_list
             return None;
         }
         let states_rest = &states[end_excl..];
-        // println!("{indent}  Matches, - goto inner -");
-        Some(get_combs(states_rest, lengths_rest, dp+4))
+        Some(get_combs(states_rest, lengths_rest))
     });
-    let r = possiblities_it.sum();
-    // println!("{indent}Ret: {}", r);
-    r
+    possiblities_it.sum()
 }
 
 fn handle_line(line: &Line) -> usize {
-    let ret = get_combs(&line.states, &line.nums, 0);
-    println!("{:?} => {}", line, ret);
-    ret
+    get_combs(&line.states, &line.nums)
 }
 
 fn part1() {
@@ -145,9 +127,6 @@ fn part1() {
         .filter(|x| x.len() > 0)
         .collect();
     let lines_v = lines.into_iter().map(parse_line).collect_vec();
-    // let f = handle_line(&lines_v[5]);
-    // println!("{}", f);
-    // return;
     let out: usize = lines_v.iter().map(handle_line).sum();
     println!("Part1: {}", out);
 }
